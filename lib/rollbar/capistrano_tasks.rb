f6d63bd74dda06995cfe0d9626c8cd15e4e2552d
therusskiy <dimanrussian@gmail.com>
2019-02-23 23:17:50 +0400

    cap sourcemaps fixes

--- lib/rollbar/capistrano_tasks.rb
+++ lib/rollbar/capistrano_tasks.rb
@@ -1,6 +1,7 @@
 require 'capistrano'
 require 'capistrano/version'
 require 'rollbar/deploy'
+require 'json'
 
 module Rollbar
   # Module containing the logic of Capistrano tasks for deploy tracking
@@ -27,6 +28,7 @@ module Rollbar
       def deploy_succeeded(capistrano, logger, dry_run)
         deploy_update(capistrano, logger, dry_run, :desc => 'Setting deployment status to `succeeded` in Rollbar') do
           report_deploy_succeeded(capistrano, dry_run)
+          upload_sourcemaps(capistrano) if capistrano.fetch(:rollbar_sourcemaps_minified_url_base)
         end
       end
 
@@ -125,6 +127,31 @@ module Rollbar
       def info_request_response(logger, result)
         logger.info result[:request_info]
         logger.info result[:response_info] if result[:response_info]
+      end
+
+      def upload_sourcemaps(capistrano)
+        url_base = capistrano.fetch(:rollbar_sourcemaps_minified_url_base)
+        url_base = "http://#{url_base}" unless url_base.index(/https?:\/\//)
+        capistrano.within capistrano.release_path do
+          capistrano.within 'public' do
+            source_maps = capistrano.capture(:find, '-name', "'*.js.map'").split("\n")
+            source_maps = source_maps.map { |file| file.gsub(/^\.\//, '') }
+            source_maps.each do |source_map|
+              minified_url = File.join(url_base, source_map)
+              args = *%W(--silent https://api.rollbar.com/api/1/sourcemap -F access_token=#{capistrano.fetch(:rollbar_token)} -F version=#{capistrano.fetch(:rollbar_revision)} -F minified_url=#{minified_url} -F source_map=@./#{source_map})
+              info "curl #{args.join(' ')}" # log the command, since capture doesn't output anything
+              api_response_body = capture(:curl, args)
+              begin
+                api_response_json = JSON.parse(api_response_body)
+                if api_response_json["err"] != 0
+                  warn "Error uploading sourcemaps: #{api_response_json["message"] || 'Unknown Error'}"
+                end
+              rescue JSON::ParserError => e
+                warn "Error parsing response: #{e.message}. Response body: #{api_response_body}"
+              end
+            end
+          end
+        end
       end
     end
   end
